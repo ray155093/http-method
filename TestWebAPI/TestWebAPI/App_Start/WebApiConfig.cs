83441af7f005d844dda39033179936f708c0d67f
ray155093 <ray155093@gmail.com>
2016-05-18 15:50:04 +0800

    之前做的httpmethod報告

---
+++ TestWebAPI/TestWebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace TestWebAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API 設定和服務
+
+            // Web API 路由
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DeleteApi",
+                routeTemplate: "api/{controller}/{value}",
+                defaults: new { name = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "InsertApi",
+                routeTemplate: "api/{controller}/{value}",
+                defaults: new { name = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "UpdateApi",
+                routeTemplate: "api/{controller}/{id}/{value}",
+                defaults: new { name = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ TestWebAPI/TestWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Collections;
+
+namespace TestWebAPI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        
+       
+        public static List<string> Result = new List<string> { "value1", "value2", "value3", "value4", "value5", "value6" };
+        
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return Result;
+        }
+
+        // GET api/values/id
+        /// <summary>
+        /// memo
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public string GetByID(  int id)
+        {
+            return Result[id];
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IEnumerable<string> Post(string value)
+        {
+            Result.Add(value);
+
+            return Result;
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        public IEnumerable<string> Put(int id, string value)
+        {
+            Result[id] = value;
+            return Result;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete]
+        public IEnumerable<string> DeleteByID(int id)
+        {
+            Result[id].Remove(id);
+            return Result;
+        }
+       
+    }
+}
